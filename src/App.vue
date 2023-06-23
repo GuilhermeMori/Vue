124741735f844d036ff11d59b07c2ca193ab0fe2
GuilhermeMori <guilhermebmori@hotmail.com>
2023-06-23 05:38:42 -0300

    Events

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,16 @@
 <template>
   <div>
-    <div>
-      One-way data binding <br />
-      Two-way data binding <br />
-      v-model -> formulários
-    </div>
+    <button @click.once="onClick">Enviar</button>
+    <br />
+
+    <div @mouseover="onMouseOver" @mouseout="onMouseOut">Mouse over</div>
 
     <br /><br />
 
-    <div>
-      <label>Nome</label> <br />
-      <input v-model="name" type="text" /> <br />
-      {{ name }}
-    </div>
-
-    <br /><br />
-
-    <div>
-      <label>Sports</label> <br />
-      <select v-model="sports">
-        <option value="">Escolha</option>
-        <option value="futebol">Futebol</option>
-        <option value="skate">Skate</option>
-        <option value="Tenis">Tenis</option>
-      </select>
-      <br />
-      {{ sports }}
-    </div>
-
-    <br /><br />
-
-    <div>
-      <label>Newsletter</label> <br />
-      <input v-model="newsletter" type="radio" value="Sim" /> Sim
-
-      <input v-model="newsletter" type="radio" value="Não" /> Não <br />
-      {{ newsletter }}
-    </div>
-
-    <br /><br />
-
-    <div>
-      <label>Contrato</label> <br />
-      <input v-model="contract" type="checkbox" /> Aceita nosso termos... <br />
-
-      {{ contract }}
-    </div>
-
-    <br /><br />
-
-    <div>
-      <label>Cores que você mais gosta</label> <br />
-      <input v-model="colors" type="checkbox" value="Azul" /> Azul
-
-      <input v-model="colors" type="checkbox" value="Amarelo" /> Amarelo <br />
-      {{ colors }}
-    </div>
+    <form action="https://google.com" @submit.prevent="onSubmit">
+      <input type="text" @keyup.enter="onKeyUp" />
+      <button type="submit">Enviar</button>
+    </form>
   </div>
 </template>
 
@@ -63,13 +18,25 @@
 export default {
   name: "App",
   data() {
-    return {
-      name: "jon snow",
-      sports: "futebol",
-      newsletter: "Não",
-      contract: true,
-      colors: [],
-    };
+    return {};
+  },
+
+  methods: {
+    onClick($evt) {
+      console.log("click", $evt);
+    },
+    onMouseOver($evt) {
+      console.log("mouse over", $evt);
+    },
+    onMouseOut($evt) {
+      console.log("mouse out", $evt);
+    },
+    onSubmit($evt) {
+      console.log("submit", $evt);
+    },
+    onKeyUp($evt) {
+      console.log("onKeyUp", $evt);
+    },
   },
 };
 </script>
